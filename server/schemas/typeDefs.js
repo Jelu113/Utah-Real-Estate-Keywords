383fccd51f93739025dc3ef5056f7687182c8e9d
Salvador M <105398955+MrPolkadot@users.noreply.github.com>
2023-11-21 20:46:02 -0700

    Fixed lawSection seeds(dashes to underscore)

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -29,6 +29,26 @@ const typeDefs = gql`
         law_section_e: String
         law_section_f: String
         law_section_g: String
+        law_section_h: String
+        law_section_i: String
+        law_section_j: String
+        law_section_k: String
+        law_section_l: String
+        law_section_m: String
+        law_section_n: String
+        law_section_o: String
+        law_section_p: String
+        law_section_q: String
+        law_section_r: String
+        law_section_s: String
+        law_section_t: String
+        law_section_u: String
+        law_section_v: String
+        law_section_w: String
+        law_section_x: String
+        law_section_y: String
+        law_section_z: String
+        
     }
 
     type LawSection {
