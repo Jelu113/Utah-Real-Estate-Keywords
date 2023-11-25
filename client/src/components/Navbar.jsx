c5cdee6d155df6c851d8f9e09b504a22cca5911a
Salvador M <105398955+MrPolkadot@users.noreply.github.com>
2023-11-25 10:52:14 -0700

    Added logout button

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,22 +8,36 @@ import {
   Tab,
   TabPanel,
   TabIndicator,
+  Button,
 } from "@chakra-ui/react";
+
+import Auth from "../utils/auth";
 
 const Navbar = () => {
   return (
     <>
       <Tabs position="relative" variant="unstyled">
-        <TabList>
+        <TabList mt="5px">
           <Tab as={Link} to="/">
             Home
           </Tab>
-          <Tab as={Link} to="/create-user">
-            Create User
-          </Tab>
-          <Tab as={Link} to="/login">
-            Login
-          </Tab>
+          {Auth.loggedIn() ? (
+            <>
+              <Tab as={Link} to="/create-keyword">
+                Create a Keyword
+              </Tab>
+              <Button onClick={Auth.logout}>Logout</Button>
+            </>
+          ) : (
+            <>
+              <Tab as={Link} to="/create-user">
+                Create User
+              </Tab>
+              <Button as={Link} to="/login">
+                Login
+              </Button>
+            </>
+          )}
         </TabList>
         <TabIndicator
           mt="-1.5px"
