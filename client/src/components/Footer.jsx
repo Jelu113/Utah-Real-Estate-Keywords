4a7aed6fef2ecd7ed01b3d2d52f1893378967a9c
Jenae Luthi <jenaeluthi@gmail.com>
2023-11-28 10:48:02 -0700

    added some style to the footer and made it dynamic. It should be at the bottom of the page once we put the other styling in.

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,26 +3,45 @@ import React from "react";
 function Footer() {
   return (
     <div className="footerContainer">
-    <footer className={"footer"}>
-       <div className = "icons">
-      
-      <a className="footerLinks"
-        href="https://le.utah.gov/xcode/Title61/Chapter2F/61-2f.html?v=C61-2f_1800010118000101"
-        target="_blank"
-        rel="noreferrer"
-       > Utah State Legislature Real Estate Code
-      </a> <br></br>
-      <a className="footerLinks"
-        href="https://adminrules.utah.gov/public/rule/R162-2f/Current%20Rules"
-        target="_blank"
-        rel="noreferrer"
-       > Complete Utah Administrative Rules
-      </a>
-      </div>
-      <p>&copy; {new Date().getFullYear()} Jenae Luthi, Salvador Mejia, JJ Saoit, Jon Bay</p>
-    </footer>
+      <footer className={"footer"}>
+        <div className="contentWrapper">
+          <div className="footerLinksContainer">
+            <a
+              className="footerLinks"
+              href="https://le.utah.gov/xcode/Title61/Chapter2F/61-2f.html?v=C61-2f_1800010118000101"
+              target="_blank"
+              rel="noreferrer"
+            >
+             <p className= "linkStyle"> Utah State Legislature Real Estate Code</p>
+            </a>
+            <a
+              className="footerLinks"
+              href="https://adminrules.utah.gov/public/rule/R162-2f/Current%20Rules"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <p className= "linkStyle">Complete Utah Administrative Rules</p>
+            </a>
+            <a
+              className="footerLinks"
+              href="https://realestate.utah.gov/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <p className= "linkStyle">Utah Division of Real Estate</p>
+            </a>
+          </div>
+          <div className="centeredText">
+            <p className= "date">
+              &copy; {new Date().getFullYear()} Jenae Luthi, Salvador Mejia, JJ
+              Saoit, Jon Bay
+            </p>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
 
 export default Footer;
+
