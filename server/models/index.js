5532fc64b433a97117b3c2f0cb723926aa9dd280
Salvador M <105398955+MrPolkadot@users.noreply.github.com>
2023-11-28 20:59:29 -0700

    resolved

--- server/models/index.js
+++ server/models/index.js
@@ -2,5 +2,5 @@ const Section = require("./Section");
 const Laws = require("./Laws");
 const Sublaws = require("./Sublaws");
 const User = require("./User");
-const Keyword = require("./keyword");
+const Keyword = require("./Keyword");
 module.exports = { Section, Laws, Sublaws, User, Keyword };
