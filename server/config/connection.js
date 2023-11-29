a844a902952d1c7809cb926c2a928f500f7d968c
Jenae Luthi <jenaeluthi@gmail.com>
2023-11-28 19:36:53 -0700

    changed URI to URL in connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const { connect, connection } = require('mongoose');
 
-const mongoURI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/realEstateLawsDB';
-connect(mongoURI);
+const mongoURL = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/realEstateLawsDB';
+connect(mongoURL);
 
 module.exports = connection;
 
