0b02f5eebfad03d25423d5ecf294e8cadcf52775
Jenae Luthi <jenaeluthi@gmail.com>
2023-11-28 18:12:25 -0700

    changed the connection.js file

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,7 @@
 const { connect, connection } = require('mongoose');
 
-connect('mongodb://127.0.0.1:27017/realEstateLawsDB');
+const mongoURI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/realEstateLawsDB';
+connect(mongoURI);
 
 module.exports = connection;
+
