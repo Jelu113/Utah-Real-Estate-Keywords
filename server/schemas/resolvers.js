9f702b8e857913edba02a2b78a7df0f9f64f2cfe
jj <saoitjensen@gmail.com>
2023-11-30 19:43:24 -0700

    fixed keyword query

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,56 +2,53 @@ const { Keyword, Section, User } = require("../models");
 const { signToken, AuthenticationError } = require("../utils/auth");
 
 const resolvers = {
-    Query: {
-        //returns all users
-        users: async () => {
-            return User.find();
-        },
-        //returns all keywords
-        keyword: async () => {
-            return Keyword.find();
-        },
-        // //lawssection
-        // lawSection: async (parent, { _id }) => {
-        //     return Section.findOne({ _id: _id });
-        // },
-        section: async () => {
-            return Section.find();
-        }
+  Query: {
+    //returns all users
+    users: async () => {
+      return User.find();
     },
-    Mutation: {
-        login: async (parent, { email, password }) => {
-            const user = await User.findOne({ email });
-
-            if (!user) {
-                throw AuthenticationError;
-            }
-
-            const correctPw = await user.isCorrectPassword(password);
-
-            if (!correctPw) {
-                throw AuthenticationError;
-            }
-
-            const token = signToken(user);
-
-            return { token, user };
-        },
-        addUser: async (parent, { username, email, password }) => {
-            const user = await User.create({ username, email, password });
-            const token = signToken(user);
-            return { token, user };
-        },
-        addKeyword: async (parent, { keyword }) => {
-            return Keyword.create({ keyword });
-        },
-        removeKeyword: async (parent, { keywordId }) => {
-            return Keyword.findOneAndDelete({ _id: keywordId })
-        },
-
+    //returns all keywords
+    keyword: async () => {
+      return Keyword.find();
     },
-}
+    // //lawssection
+    // lawSection: async (parent, { _id }) => {
+    //     return Section.findOne({ _id: _id });
+    // },
+    section: async () => {
+      return Section.find();
+    },
+  },
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
 
+      if (!user) {
+        throw AuthenticationError;
+      }
 
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw AuthenticationError;
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    addKeyword: async (parent, { keyword }) => {
+      return Keyword.create({ keyword });
+    },
+    removeKeyword: async (parent, { keywordId }) => {
+      return Keyword.findOneAndDelete({ _id: keywordId });
+    },
+  },
+};
 
 module.exports = resolvers;
